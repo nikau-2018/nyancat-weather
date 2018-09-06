34d6ec3f06684f7202960356123af8880f489206
Alisa Herrera Hayman <alisamhh@gmail.com>
2018-09-06 18:06:43 +1200

    Working on horoscope API

---
+++ client/api.js
@@ -0,0 +1,15 @@
+import request from 'superagent'
+
+// const horoscopeApi = 'https://www.horoscopes-and-astrology.com/json'
+
+// function getHoroscopes () {
+//   return request
+//     .get(horoscopeApi)
+//     .catch(err => {
+//       console.log(err.message)
+//     })
+// }
+
+export default {
+  // getHoroscopes
+}
